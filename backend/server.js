669253548a4fb876a96806397213f08b6ca18f0f
Reyy <rheina putri ferdiansyah>
2022-02-13 13:53:20 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const jsonParser = express.json();
+const urlEncodedParser = express.urlencoded();
+app.use(jsonParser);
+app.use(urlEncodedParser);
+app.use(cors())
+
+const member = require("./router/member")
+const user = require("./router/user")
+const paket = require("./router/paket")
+const transaksi = require("./router/transaksi")
+const { login } = require("./router/login")
+ 
+app.use("/laundry/api/auth", login)
+app.use("/laundry/api/transaksi", transaksi)
+app.use("/laundry/api/paket", paket)
+app.use("/laundry/api/user", user)
+app.use("/laundry/api/member", member)
+
+app.use(express.static(__dirname))
+
+app.listen(8000, () => {
+    console.log('Server run on port 8000')
+})
